1cb83ef1aa7fd054b660ce7b14f97f146f08398c
del <fadel@dels-MacBook-Pro.local>
2022-03-19 20:27:19 +0700

    update index, explore and headnav

--- pages/explore.js
+++ pages/explore.js
@@ -20,69 +20,71 @@ export default function Explore({ posts }) {
 		<>
 			<div className="w-full bg-gradient-to-br from-white to-gray-200 text-gray-900">
 				<HeadNav />
-				<section className="mx-auto flex max-w-screen-xl justify-between py-28 px-8">
-					<div className="max-w-2xl">
-						<div className="flex">
-							<h1 className="rounded-xl bg-orange-500 px-6 py-3 text-white shadow-lg">
-								#FreedomOfSearch
+				<div className="bg-gradient-to-b from-gray-100 to-gray-200 w-full">
+					<section className="mx-auto flex max-w-screen-xl justify-between py-28 px-8">
+						<div className="max-w-2xl">
+							<div className="flex">
+								<h1 className="rounded-xl bg-orange-500 px-6 py-3 text-white shadow-lg">
+									#FreedomOfSearch
+								</h1>
+							</div>
+							<h1 className="mt-8 text-4xl font-bold leading-tight">
+								Find the perfect freelance service for your business
 							</h1>
+							<p className="mt-8 max-w-md font-light leading-relaxed text-gray-500">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi
+								viverra lorem vulputate luctus. Sollicitudin vitae nulla amet
+								pellentesque. Nunc magna mauris risus bibendum et, malesuada
+								viverra nisl. Facilisis metus purus.
+							</p>
 						</div>
-						<h1 className="mt-8 text-4xl font-bold leading-tight">
-							Find the perfect freelance service for your business
-						</h1>
-						<p className="mt-8 max-w-md font-light leading-relaxed text-gray-500">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi
-							viverra lorem vulputate luctus. Sollicitudin vitae nulla amet
-							pellentesque. Nunc magna mauris risus bibendum et, malesuada
-							viverra nisl. Facilisis metus purus.
-						</p>
-					</div>
-					<div className="flex flex-1 flex-col">
-						{/* <Image
+						<div className="flex flex-1 flex-col">
+							{/* <Image
 						className="max-w-lg self-end shadow-2xl"
 						src={HeroImage}
 						alt="hero"
 					/> */}
-					</div>
-				</section>
-				<section className="mx-auto max-w-screen-xl py-28 px-8">
-					<div className="flex items-baseline justify-between">
-						<h1 className=" text-4xl font-bold">Top trending in 3 days</h1>
-						<Link href="/explore/all">
-							<a className="text-lg text-gray-500 hover:text-gray-900 transition duration-150">
-								View Collections
-							</a>
-						</Link>
-					</div>
-					<div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
-						{posts.map((post) => (
-							<div key={post.id}>
-								<ProductCard post={post} />
-							</div>
-						))}
-					</div>
-				</section>
+						</div>
+					</section>
+					<section className="mx-auto max-w-screen-xl py-28 px-8">
+						<div className="flex items-baseline justify-between">
+							<h1 className=" text-4xl font-bold">Top trending in 3 days</h1>
+							<Link href="/explore/all">
+								<a className="text-lg text-gray-500 hover:text-gray-900 transition duration-150">
+									View Collections
+								</a>
+							</Link>
+						</div>
+						<div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
+							{posts.map((post) => (
+								<div key={post.id}>
+									<ProductCard post={post} />
+								</div>
+							))}
+						</div>
+					</section>
 
-				<section className="mx-auto max-w-screen-xl py-28 px-8">
-					<div className="flex items-baseline justify-between">
-						<h1 className=" text-4xl font-bold">
-							Recommended based on your interests
-						</h1>
-						<Link href="/explore/all">
-							<a className="text-lg text-gray-500 hover:text-gray-900 transition duration-150">
-								View Collections
-							</a>
-						</Link>
-					</div>
-					<div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
-						{posts.map((post) => (
-							<div key={post.id}>
-								<ProductCard post={post} />
-							</div>
-						))}
-					</div>
-				</section>
-				<FootNav />
+					<section className="mx-auto max-w-screen-xl py-28 px-8">
+						<div className="flex items-baseline justify-between">
+							<h1 className=" text-4xl font-bold">
+								Recommended based on your interests
+							</h1>
+							<Link href="/explore/all">
+								<a className="text-lg text-gray-500 hover:text-gray-900 transition duration-150">
+									View Collections
+								</a>
+							</Link>
+						</div>
+						<div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
+							{posts.map((post) => (
+								<div key={post.id}>
+									<ProductCard post={post} />
+								</div>
+							))}
+						</div>
+					</section>
+					<FootNav />
+				</div>
 			</div>
 		</>
 	);
--- pages/index.js
+++ pages/index.js
@@ -35,118 +35,95 @@ export async function getStaticProps() {
 export default function index({ posts }) {
 	return (
 		<>
-			<div className="w-full bg-gradient-to-br from-white to-gray-200 text-gray-900">
+			<div className="w-full text-gray-900">
 				<HeadNav />
-				<section className="mx-auto flex max-w-screen-xl justify-between py-28 px-8">
-					<div className="max-w-2xl">
-						<div className="relative w-2/3">
-							<input
-								id="search"
-								type="search"
-								className="focus:shadow-outline w-full rounded-lg py-4 px-6 text-sm font-medium leading-none text-gray-800 placeholder-gray-400 shadow-md transition duration-150 hover:shadow-lg focus:border-0 focus:shadow-lg focus:outline-none"
-								placeholder="Search for a service"
+				<div className="bg-gradient-to-b from-gray-100 to-gray-200 w-full">
+					<section className="mx-auto flex max-w-screen-xl justify-between py-28 px-8">
+						<div className="max-w-2xl">
+							<h1 className="text-6xl font-bold leading-tight">
+								Hire best freelancer for any job, online
+							</h1>
+							<p className="mt-8 max-w-md font-light leading-relaxed text-gray-500">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi
+								viverra lorem vulputate luctus. Sollicitudin vitae nulla amet
+								pellentesque. Nunc magna mauris risus bibendum et, malesuada
+								viverra nisl. Facilisis metus purus.
+							</p>
+							<div className="mt-12 space-x-8">
+								<Link href="/login">
+									<a className="rounded-2xl bg-orange-500 px-6 py-3.5 font-medium text-white transition duration-150 hover:bg-orange-600 hover:shadow-lg hover:shadow-orange-200">
+										Get Started
+									</a>
+								</Link>
+								<Link href="/signup">
+									<a className="rounded-2xl border border-orange-500 px-6 py-3.5 font-medium text-orange-500 transition duration-150 hover:shadow-xl">
+										Learn More
+									</a>
+								</Link>
+							</div>
+						</div>
+						<div className="flex flex-1 flex-col">
+							<Image
+								className="max-w-lg self-end shadow-2xl"
+								src={HeroImage}
+								alt="hero"
 							/>
-							<div className="absolute right-5 top-3.5 cursor-pointer">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-5 w-5"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									strokeWidth={2}
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-									/>
-								</svg>
-							</div>
 						</div>
-
-						<h1 className="mt-8 text-6xl font-bold leading-tight">
-							Hire best freelancer for any job, online
-						</h1>
-						<p className="mt-8 max-w-md font-light leading-relaxed text-gray-500">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi
-							viverra lorem vulputate luctus. Sollicitudin vitae nulla amet
-							pellentesque. Nunc magna mauris risus bibendum et, malesuada
-							viverra nisl. Facilisis metus purus.
-						</p>
-						<div className="mt-12 space-x-8">
-							<Link href="/login">
-								<a className="rounded-2xl bg-orange-500 px-6 py-3.5 font-medium text-white transition duration-150 hover:bg-orange-600 hover:shadow-lg hover:shadow-orange-200">
-									Get Started
-								</a>
-							</Link>
-							<Link href="/signup">
-								<a className="rounded-2xl border border-orange-500 px-6 py-3.5 font-medium text-orange-500 transition duration-150 hover:shadow-xl">
-									Learn More
-								</a>
-							</Link>
+					</section>
+					<section className="mx-auto flex max-w-screen-xl items-center justify-between py-28 px-8">
+						<ProductCard post={posts[2]} />
+						<div className="flex flex-col items-start space-y-4">
+							<h1 className="rounded-xl bg-orange-500 px-6 py-3 text-white shadow-lg">
+								#OurAdvantage
+							</h1>
+							<h1 className="max-w-xl text-4xl font-bold">
+								A whole world of freelance talent at your fingertips
+							</h1>
+							<p className="mt-8 max-w-md font-light leading-relaxed text-gray-500">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi
+								viverra lorem vulputate luctus. Sollicitudin vitae nulla amet
+								pellentesque.
+							</p>
 						</div>
-					</div>
-					<div className="flex flex-1 flex-col">
-						<Image
-							className="max-w-lg self-end shadow-2xl"
-							src={HeroImage}
-							alt="hero"
-						/>
-					</div>
-				</section>
-				<section className="mx-auto flex max-w-screen-xl items-center justify-between py-28 px-8">
-					<ProductCard post={posts[2]} />
-					<div className="flex flex-col items-start space-y-4">
-						<h1 className="rounded-xl bg-orange-500 px-6 py-3 text-white shadow-lg">
-							#OurAdvantage
-						</h1>
-						<h1 className="max-w-xl text-4xl font-bold">
-							A whole world of freelance talent at your fingertips
-						</h1>
-						<p className="mt-8 max-w-md font-light leading-relaxed text-gray-500">
+					</section>
+					<section className="mx-auto flex max-w-screen-xl flex-1 flex-col items-center justify-center py-28 px-8">
+						<h1 className="max-w-xl text-4xl font-bold">Why choose us?</h1>
+						<p className="mt-8 max-w-md text-center font-light leading-relaxed text-gray-500">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi
-							viverra lorem vulputate luctus. Sollicitudin vitae nulla amet
-							pellentesque.
+							viverra lorem vulputate luctus.
 						</p>
-					</div>
-				</section>
-				<section className="mx-auto flex max-w-screen-xl flex-1 flex-col items-center justify-center py-28 px-8">
-					<h1 className="max-w-xl text-4xl font-bold">Why choose us?</h1>
-					<p className="mt-8 max-w-md text-center font-light leading-relaxed text-gray-500">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi viverra
-						lorem vulputate luctus.
-					</p>
-					<div className="mt-20 flex w-full justify-between gap-8">
-						<ReviewCard />
-						<div className="mb-40">
+						<div className="mt-20 flex w-full justify-between gap-8">
 							<ReviewCard />
-						</div>
-						<ReviewCard />
-						{/* className="-translate-y-12 transform" */}
-					</div>
-				</section>
-				<section className="mx-auto flex max-w-screen-xl flex-1 flex-col items-center justify-center space-y-8 py-28 px-8">
-					<h1 className="max-w-xl text-4xl font-bold">Review Clients</h1>
-					<div className="mt-4 flex items-center justify-center -space-x-2">
-						{connections.map((conn) => (
-							<div className="h-20 w-20 rounded-full shadow" key={conn.id}>
-								<Image
-									alt="img"
-									src={conn.img}
-									height={200}
-									width={200}
-									className="h-full w-full rounded-full object-cover object-center"
-								/>
+							<div className="mb-40">
+								<ReviewCard />
 							</div>
-						))}
-					</div>
-					<h1 className="mt-4 max-w-xl text-center text-sm font-normal text-gray-500">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat.
-					</h1>
-				</section>
+							<ReviewCard />
+							{/* className="-translate-y-12 transform" */}
+						</div>
+					</section>
+					<section className="mx-auto flex max-w-screen-xl flex-1 flex-col items-center justify-center space-y-8 py-28 px-8">
+						<h1 className="max-w-xl text-4xl font-bold">Review Clients</h1>
+						<div className="mt-4 flex items-center justify-center -space-x-2">
+							{connections.map((conn) => (
+								<div className="h-20 w-20 rounded-full shadow" key={conn.id}>
+									<Image
+										alt="img"
+										src={conn.img}
+										height={200}
+										width={200}
+										className="h-full w-full rounded-full object-cover object-center"
+									/>
+								</div>
+							))}
+						</div>
+						<h1 className="mt-4 max-w-xl text-center text-sm font-normal text-gray-500">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat.
+						</h1>
+					</section>
+				</div>
 				<FootNav />
 			</div>
 		</>
