59f19a33f0cb14d7309447056cc4e6367a414746
del <fadel@dels-MacBook-Pro.local>
2022-03-19 20:52:24 +0700

    add about page and update footnav

--- components/FootNav.js
+++ components/FootNav.js
@@ -6,10 +6,15 @@ import {
 	CubeTransparentIcon,
 } from "@heroicons/react/solid";
 
+import Image from "next/image";
+import TwitterIcon from "../public/Twitter.svg";
+import LinkedinIcon from "../public/Linkedin.svg";
+import FacebookIcon from "../public/Facebook.svg";
+
 export default function FootNav() {
 	return (
-		<section className="mt-20 w-full bg-white">
-			<div className="container mx-auto grid grid-cols-3 gap-8 max-w-screen-xl items-start justify-between p-8 text-gray-700">
+		<section className="mt-4 w-full bg-white">
+			<div className="container mx-auto grid grid-cols-3 gap-8 max-w-screen-xl items-center justify-between p-8 text-gray-700">
 				<div className="flex-cols justify-center items-center ml-8">
 					<h1 className="mb-4 font-medium">SnapWork</h1>
 					<p className="max-w-xs font-light text-gray-500">
@@ -17,12 +22,22 @@ export default function FootNav() {
 						possible.
 					</p>
 				</div>
-				<div className="flex justify-center items-center space-x-8">
-					<MapIcon className="h-10 w-10" />
-					<ShoppingCartIcon className="h-10 w-10" />
-					<BookmarkIcon className="h-10 w-10" />
+				<div className="flex justify-center items-center gap-6">
+					<Image src={TwitterIcon} width={35} height={35} alt="Twitter Icon" />
+					<Image
+						src={LinkedinIcon}
+						width={35}
+						height={35}
+						alt="Linkedin Icon"
+					/>
+					<Image
+						src={FacebookIcon}
+						width={35}
+						height={35}
+						alt="Facebook Icon"
+					/>
 				</div>
-				<div className="flex-cols justify-center items-center ml-8">
+				<div className="flex-cols justify-center items-center ml-20">
 					<h1 className="mb-4 font-medium">Support</h1>
 					<p className="font-light text-gray-500">Terms and Conditions</p>
 					<p className="font-light text-gray-500">Privacy Policy</p>
---
+++ pages/about.js
@@ -0,0 +1,107 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import Image from "next/image";
+import HeadNav from "../components/HeadNav";
+import FootNav from "../components/FootNav";
+
+export default function about() {
+	return (
+		<body>
+			<HeadNav />
+			<div className="w-full bg-gradient-to-b from-gray-100 to-gray-200">
+				<div className="py-9 px-4 md:py-12 md:px-6 lg:py-16 lg:px-20 2xl:container 2xl:mx-auto">
+					<div className="flex flex-col justify-between gap-8 lg:flex-row">
+						<div className="flex w-full flex-col justify-center lg:w-5/12">
+							<h1 className="pb-4 text-3xl font-bold leading-9 text-gray-800 lg:text-4xl">
+								About Us
+							</h1>
+							<p className="text-base font-normal leading-6 text-gray-600">
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+								The point of using Lorem Ipsum.In the first place we have
+								granted to God, and by this our present charter confirmed for us
+								and our heirs forever that the English Church shall be free, and
+								shall have her rights entire, and her liberties inviolate; and
+								we will that it be thus observed; which is apparent from
+							</p>
+						</div>
+						<div className="w-full lg:w-8/12">
+							<img
+								className="h-full w-full"
+								src="https://i.ibb.co/FhgPJt8/Rectangle-116.png"
+								alt="A group of People"
+							/>
+						</div>
+					</div>
+
+					<div className="flex flex-col justify-between gap-8 pt-12 lg:flex-row">
+						<div className="flex w-full flex-col justify-center lg:w-5/12">
+							<h1 className="pb-4 text-3xl font-bold leading-9 text-gray-800 lg:text-4xl">
+								Our Story
+							</h1>
+							<p className="text-base font-normal leading-6 text-gray-600">
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+								The point of using Lorem Ipsum.In the first place we have
+								granted to God, and by this our present charter confirmed for us
+								and our heirs forever that the English Church shall be free, and
+								shall have her rights entire, and her liberties inviolate; and
+								we will that it be thus observed; which is apparent from
+							</p>
+						</div>
+						<div className="w-full lg:w-8/12 lg:pt-8">
+							<div className="bg-gray-100 grid grid-cols-1 rounded-md shadow-md sm:grid-cols-2 md:grid-cols-3 lg:gap-4">
+								<div className="flex flex-col items-center justify-center p-4 pb-6">
+									<img
+										className="hidden md:block"
+										src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png"
+										alt="Alexa featured Image"
+									/>
+									<img
+										className="block md:hidden"
+										src="https://i.ibb.co/zHjXqg4/Rectangle-118.png"
+										alt="Alexa featured Image"
+									/>
+									<p className="mt-4 text-xl font-medium leading-5 text-gray-800">
+										Alexa
+									</p>
+								</div>
+								<div className="flex flex-col items-center justify-center p-4 pb-6">
+									<img
+										className="hidden md:block"
+										src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png"
+										alt="Liam featued Image"
+									/>
+									<img
+										className="block md:hidden"
+										src="https://i.ibb.co/C5MMBcs/Rectangle-120.png"
+										alt="Liam featued Image"
+									/>
+									<p className="mt-4 text-xl font-medium leading-5 text-gray-800">
+										Liam
+									</p>
+								</div>
+								<div className="flex flex-col items-center justify-center p-4 pb-6">
+									<img
+										className="hidden md:block"
+										src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png"
+										alt="Elijah featured image"
+									/>
+									<img
+										className="block md:hidden"
+										src="https://i.ibb.co/ThZBWxH/Rectangle-121.png"
+										alt="Elijah featured image"
+									/>
+									<p className="mt-4 text-xl font-medium leading-5 text-gray-800">
+										Elijah
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<FootNav />
+		</body>
+	);
+}
