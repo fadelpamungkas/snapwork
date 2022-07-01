35835e5a8d86bd844718ef5856817bde0c8c85d9
fadelpamungkas <fadelpamungkas3@gmail.com>
2022-07-01 22:51:43 +0700

    fix profiltab fontsize

--- components/ProfilTab.js
+++ components/ProfilTab.js
@@ -3,7 +3,7 @@ export default function ProfilTab() {
     <>
       <div className="p-4">
         <div className="bg-yellow-100 rounded-lg p-4">
-          <h1 className="text-xs text-yellow-700">Hai mikaela, selamat datang di aplikasi SnapWork Saat ini profil cv kamu belum lengkap, Ayo update cv kamu dari sekarang, agar Kamu lebih mudah mencari pekerjaan yang paling tepat. Biarkan perusahaan tahu kualifikasi dan prestasi terbaru kamu melalui cv!</h1>
+          <h1 className="text-sm text-yellow-700">Hai mikaela, selamat datang di aplikasi SnapWork Saat ini profil cv kamu belum lengkap, Ayo update cv kamu dari sekarang, agar Kamu lebih mudah mencari pekerjaan yang paling tepat. Biarkan perusahaan tahu kualifikasi dan prestasi terbaru kamu melalui cv!</h1>
         </div>
         <button className="bg-white shadow-lg w-full border text-sm py-2.5 my-4 rounded-lg">Kelola CV</button>
       </div>
