171cb39d8c9bcf9cc8066dd88fb34372a98dc5ac
fadelpamungkas <fadelpamungkas3@gmail.com>
2022-10-05 19:56:53 +0700

    Improve company page

--- components/EditableCareerCard.js
+++ components/EditableCareerCard.js
@@ -1,7 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
+import EditLowonganDialog from "../components/EditLowonganDialog";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
 import TokopediaAvatar from "../public/avtokopedia.png";
 export default function EditableCareerCard() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
   return (
     <>
       <div className="w-full bg-white rounded-xl shadow-xl">
@@ -57,8 +69,76 @@ Membuat aplikasi atau situs yang mudah digunakan oleh pengguna, dan terlihat ker
               <h1 className="text-xl text-white">Lowongan</h1>
             </div>
           </div>
+          <div className="absolute right-0 bottom-0">
+            <div className="p-4">
+              <button
+                type="button"
+                className="inline-flex justify-center py-2 px-8 font-medium text-white bg-green-500 rounded-md border border-transparent transition duration-150 hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                onClick={openModal}
+              >
+                Detail
+              </button>
+            </div>
+          </div>
         </div>
       </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="overflow-y-auto fixed inset-0">
+            <div className="flex justify-center items-center p-4 min-h-full text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="overflow-hidden p-8 w-full max-w-6xl text-left align-middle bg-white rounded-2xl transition-all transform">
+                  <Dialog.Title
+                    as="h3"
+                    className="p-4 text-xl font-medium leading-6 text-center text-gray-900"
+                  >
+                    Ubah Lowongan
+                  </Dialog.Title>
+                  <hr />
+                  <EditLowonganDialog />
+
+                  <div className="flex justify-end mt-4 space-x-8">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center py-2 px-8 font-medium text-white bg-red-500 rounded-md border border-transparent transition duration-150 hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Batal
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center py-2 px-8 font-medium text-white bg-green-500 rounded-md border border-transparent transition duration-150 hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Post
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 }
