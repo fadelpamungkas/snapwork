65aa804c486685bdcf42a1d418c79c2af96b4650
fadelpamungkas <fadelpamungkas3@gmail.com>
2022-10-07 02:51:10 +0700

    Completing Payment popup

--- components/PengembanganDiriTab.js
+++ components/PengembanganDiriTab.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import PaymentBanner from "../public/PaymentBanner1.png";
+import BNILogo from "../public/BNI.png";
+import BCALogo from "../public/BCA.png";
+import MandiriLogo from "../public/Mandiri.png";
+import GoPayLogo from "../public/GoPay.png";
 import { Tab } from "@headlessui/react";
 import { CogIcon } from "@heroicons/react/outline";
 import { Dialog, Transition } from "@headlessui/react";
@@ -13,6 +17,7 @@ export default function CVTab() {
   const { user } = useUser();
   const [edit, setEdit] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenFinal, setIsOpenFinal] = useState(false);
 
   const tabItem = [
     {
@@ -44,7 +49,17 @@ export default function CVTab() {
   }
 
   function openModal() {
+    setIsOpenFinal(false);
     setIsOpen(true);
+  }
+
+  function closeModalFinal() {
+    setIsOpenFinal(false);
+  }
+
+  function openModalFinal() {
+    setIsOpen(false);
+    setIsOpenFinal(true);
   }
   return (
     <>
@@ -295,36 +310,180 @@ laporan lengkap`}</p>
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
                     <Image
-                      className="object-fill max-w-lg rounded-2xl shadow-2xl"
+                      className="object-fill max-w-lg rounded-xl shadow-xl"
                       src={PaymentBanner}
                       alt="Header"
                     />
-                    <div className="p-4 bg-white shadow-xl rounded-2xl -translate-y-16 m-4">
+                    <div className="p-4 m-2 bg-white rounded-2xl shadow-xl -translate-y-12">
                       <h1>Total</h1>
-                      <h1 className="font-bold text-2xl">Rp50.000</h1>
-                      <h1 className="text-sm text-gray-300">Order ID #report-202210050735258297562</h1>
+                      <h1 className="text-2xl font-bold">Rp50.000</h1>
+                      <h1 className="text-sm text-gray-300">
+                        Order ID #report-202210050735258297562
+                      </h1>
                     </div>
                   </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500 whitespace-pre-line">
-                      Semua Perubahan akan disimpan dan tidak dapat
-                      dikembalikan.
-                    </p>
+                  <div className="mx-4 space-y-2">
+                    <h1 className="text-lg font-semibold">Transfer Bank</h1>
+                    <div className="space-y-4">
+                      <div className="flex justify-start items-start">
+                        <div className="p-1 border border-gray-900">
+                          <Image
+                            className="object-contain"
+                            src={BCALogo}
+                            width={50}
+                            height={20}
+                            alt="BCA"
+                          />
+                        </div>
+                        <div className="mx-4 space-y-1">
+                          <h1 className="text-3xl font-bold">
+                            1420 0008 6693 1
+                          </h1>
+                          <h1 className="text-lg font-medium">
+                            BCA {" "}
+                            <span className="font-normal text-base">atas nama</span>
+                            {" "}Fadel Pamungkas
+                          </h1>
+                        </div>
+                      </div>
+                      <hr />
+                      <div className="flex justify-start items-start">
+                        <div className="p-1 border border-gray-900">
+                          <Image
+                            className="object-contain"
+                            src={MandiriLogo}
+                            width={50}
+                            height={20}
+                            alt="Mandiri"
+                          />
+                        </div>
+                        <div className="mx-4 space-y-1">
+                          <h1 className="text-3xl font-bold">
+                            1420 0008 6693 1
+                          </h1>
+                          <h1 className="text-lg font-medium">
+                            Mandiri {" "}
+                            <span className="font-normal text-base">atas nama</span>
+                            {" "}Fadel Pamungkas
+                          </h1>
+                        </div>
+                      </div>
+                      <hr />
+                      <div className="flex justify-start items-start">
+                        <div className="p-1 border border-gray-900">
+                          <Image
+                            className="object-contain"
+                            src={BNILogo}
+                            width={50}
+                            height={20}
+                            alt="BNI"
+                          />
+                        </div>
+                        <div className="mx-4 space-y-1">
+                          <h1 className="text-3xl font-bold">
+                            1420 0008 6693 1
+                          </h1>
+                          <h1 className="text-lg font-medium">
+                            BNI {" "}
+                            <span className="font-normal text-base">atas nama</span>
+                            {" "}Haydar Maulana
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="mt-4 space-x-4 flex justify-end">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center py-2 px-4 text-sm font-medium text-blue-900 bg-blue-100 rounded-md border border-transparent hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModalAndSave}
-                    >
-                      Simpan
-                    </button>
+                  <div className="pt-8 m-4 space-y-2">
+                    <h1 className="text-lg font-semibold">Cara Pembayaran</h1>
+                    <ul className="px-4 list-decimal list-outside">
+                      <li className="">Ketuk ATM di halaman pembayaran.</li>
+                      <li className="">Ketuk Bayar dengan ATM.</li>
+                      <li className="">
+                        Pastikan untuk memasukkan kode bank, nomor rekening
+                        Snapwork, dan nominal pembayaran yang tepat, kemudian
+                        lakukan transfer
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="flex justify-end m-4 space-x-4">
                     <button
                       type="button"
                       className="inline-flex justify-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-md border border-transparent hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-200 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
                       Batal
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center py-2 px-4 text-sm font-medium text-blue-900 bg-blue-100 rounded-md border border-transparent hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={openModalFinal}
+                    >
+                      Lanjut
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+      <Transition appear show={isOpenFinal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModalFinal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="overflow-y-auto fixed inset-0">
+            <div className="flex justify-center items-center p-4 min-h-full text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="overflow-hidden p-8 w-full max-w-lg text-left align-middle bg-white rounded-2xl transition-all transform">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Terima Kasih!
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500 whitespace-pre-line">
+                      Anda berhasil mengisi lamaran pada{" "}
+                      <span className="font-bold">
+                        {" "}
+                        Application Designer - PT. Tokopedia Indonesia.{" "}
+                      </span>
+                      Anda dapat memantau proses rekrutmen melalui halaman{" "}
+                      <span className="text-blue-500"> Profil Saya.</span>
+                    </p>
+                  </div>
+
+                  <div className="flex justify-end space-x-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-md border border-transparent hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-200 focus-visible:ring-offset-2"
+                      onClick={openModal}
+                    >
+                      Kembali
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center py-2 px-4 text-sm font-medium text-blue-900 bg-blue-100 rounded-md border border-transparent hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModalFinal}
+                    >
+                      OK
                     </button>
                   </div>
                 </Dialog.Panel>
