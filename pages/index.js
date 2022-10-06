f862dacfe7c355f66dfd8a8b1ca26a1d41bae279
fadelpamungkas <fadelpamungkas3@gmail.com>
2022-10-06 15:37:59 +0700

    Add banner on beranda page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
+import HomeBanner from "../public/HomeBanner1.png";
 import HeadNav from "../components/HeadNav";
 import FootNav from "../components/FootNav";
 import CareerCard from "../components/CareerCard";
-import { Fragment, useState } from 'react'
-import { Combobox, Transition } from '@headlessui/react'
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
 
 const education = [
-  { id: 0, name: 'Semua' },
-  { id: 1, name: 'SMA/SMK' },
-  { id: 2, name: 'D3' },
-  { id: 3, name: 'S1/D4' },
-  { id: 4, name: 'S2' },
-  { id: 5, name: 'S3' },
+  { id: 0, name: "Semua" },
+  { id: 1, name: "SMA/SMK" },
+  { id: 2, name: "D3" },
+  { id: 3, name: "S1/D4" },
+  { id: 4, name: "S2" },
+  { id: 5, name: "S3" },
 ];
 
 const region = [
-  { id: 0, name: 'Semua' },
-  { id: 1, name: 'D.I.Yogyakarta' },
-  { id: 2, name: 'Jakarta' },
-  { id: 3, name: 'Surabaya' },
-  { id: 4, name: 'Bandung' },
-  { id: 5, name: 'Medan' },
+  { id: 0, name: "Semua" },
+  { id: 1, name: "D.I.Yogyakarta" },
+  { id: 2, name: "Jakarta" },
+  { id: 3, name: "Surabaya" },
+  { id: 4, name: "Bandung" },
+  { id: 5, name: "Medan" },
 ];
 
 const cbItems = [
@@ -51,47 +52,65 @@ const cbItems = [
     value: "Game Developer",
   },
 ];
-export default function Index({ }) {
-  const [selectedEducation, setSelectedEducation] = useState(education[0])
-  const [selectedRegion, setSelectedRegion] = useState(region[0])
-  const [query, setQuery] = useState('')
+export default function Index({}) {
+  const [selectedEducation, setSelectedEducation] = useState(education[0]);
+  const [selectedRegion, setSelectedRegion] = useState(region[0]);
+  const [query, setQuery] = useState("");
 
   const filteredEducation =
-    query === ''
+    query === ""
       ? education
       : education.filter((edu) =>
-        edu.name
-          .toLowerCase()
-          .replace(/\s+/g, '')
-          .includes(query.toLowerCase().replace(/\s+/g, ''))
-      )
+          edu.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
   const filteredRegion =
-    query === ''
+    query === ""
       ? region
       : region.filter((reg) =>
-        reg.name
-          .toLowerCase()
-          .replace(/\s+/g, '')
-          .includes(query.toLowerCase().replace(/\s+/g, ''))
-      )
+          reg.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
   return (
     <>
-      <div className="w-full py-8 text-gray-900">
+      <div className="py-8 w-full text-gray-900">
         <HeadNav />
         <div className="w-full bg-gray-50">
-          <section className="mx-auto max-w-screen-xl py-16 px-8 grid grid-cols-4 gap-8">
+          <section className="pt-16 px-8 mx-auto max-w-screen-xl">
+            <Image
+              className="object-fill max-w-lg rounded-2xl shadow-2xl"
+              src={HomeBanner}
+              alt="Banner"
+            />
+          </section>
+          <section className="grid grid-cols-4 gap-8 py-8 px-8 mx-auto max-w-screen-xl">
             <div className="col-span-1">
-              <div className="w-full mx-auto flex-col justify-between items-center rounded-xl bg-white shadow-lg">
-                <div className="bg-blue-500 rounded-t-xl py-3 px-6">
-                  <h1 className="text-white text-lg">Filter Pencarian</h1>
+              <div className="flex-col justify-between items-center mx-auto w-full bg-white rounded-xl shadow-lg">
+                <div className="py-3 px-6 bg-blue-500 rounded-t-xl">
+                  <h1 className="text-lg text-white">Filter Pencarian</h1>
                 </div>
-                <div className="w-full py-4 px-6">
+                <div className="py-4 px-6 w-full">
                   <div clasName="py-2">
                     <h1 className="mb-2 text-sm">Kategori</h1>
                     {cbItems.map((item) => (
-                      <div key={item.id} className="w-full flex items-center justify-start mb-2">
-                        <input id={item.id} type="checkbox" value="" className="text-blue-600 bg-gray-100 rounded border-gray-300 dark:bg-gray-700 dark:border-gray-600" />
-                        <label htmlFor={item.id} className="ml-2 text-sm text-gray-800">
+                      <div
+                        key={item.id}
+                        className="flex justify-start items-center mb-2 w-full"
+                      >
+                        <input
+                          id={item.id}
+                          type="checkbox"
+                          value=""
+                          className="text-blue-600 bg-gray-100 rounded border-gray-300 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label
+                          htmlFor={item.id}
+                          className="ml-2 text-sm text-gray-800"
+                        >
                           {item.value}
                         </label>
                       </div>
@@ -99,17 +118,20 @@ export default function Index({ }) {
                   </div>
                   <div className="py-2">
                     <h1 className="mb-2 text-sm">Jenjang Pendidikan</h1>
-                    <Combobox value={selectedEducation} onChange={setSelectedEducation}>
+                    <Combobox
+                      value={selectedEducation}
+                      onChange={setSelectedEducation}
+                    >
                       <div className="relative mt-1">
-                        <div className="relative w-full cursor-default overflow-hidden rounded bg-white text-left border border-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                        <div className="overflow-hidden relative w-full text-left bg-white rounded border border-gray-300 cursor-default sm:text-sm focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300">
                           <Combobox.Input
-                            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                            className="py-2 pr-10 pl-3 w-full text-sm leading-5 text-gray-900 border-none focus:ring-0"
                             displayValue={(edu) => edu.name}
                             onChange={(event) => setQuery(event.target.value)}
                           />
-                          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                          <Combobox.Button className="flex absolute inset-y-0 right-0 items-center pr-2">
                             <SelectorIcon
-                              className="h-5 w-5 text-gray-400"
+                              className="w-5 h-5 text-gray-400"
                               aria-hidden="true"
                             />
                           </Combobox.Button>
@@ -119,11 +141,11 @@ export default function Index({ }) {
                           leave="transition ease-in duration-100"
                           leaveFrom="opacity-100"
                           leaveTo="opacity-0"
-                          afterLeave={() => setQuery('')}
+                          afterLeave={() => setQuery("")}
                         >
-                          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {filteredEducation.length === 0 && query !== '' ? (
-                              <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                          <Combobox.Options className="overflow-auto absolute z-10 py-1 mt-1 w-full max-h-60 text-base bg-white rounded-md ring-1 ring-black ring-opacity-5 shadow-lg sm:text-sm focus:outline-none">
+                            {filteredEducation.length === 0 && query !== "" ? (
+                              <div className="relative py-2 px-4 text-gray-700 cursor-default select-none">
                                 Nothing found.
                               </div>
                             ) : (
@@ -131,7 +153,10 @@ export default function Index({ }) {
                                 <Combobox.Option
                                   key={edu.id}
                                   className={({ active }) =>
-                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                      active
+                                        ? "bg-blue-500 text-white"
+                                        : "text-gray-900"
                                     }`
                                   }
                                   value={edu}
@@ -139,17 +164,26 @@ export default function Index({ }) {
                                   {({ selected, active }) => (
                                     <>
                                       <span
-                                        className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                          }`}
+                                        className={`block truncate ${
+                                          selected
+                                            ? "font-medium"
+                                            : "font-normal"
+                                        }`}
                                       >
                                         {edu.name}
                                       </span>
                                       {selected ? (
                                         <span
-                                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-blue-500'
-                                            }`}
+                                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                            active
+                                              ? "text-white"
+                                              : "text-blue-500"
+                                          }`}
                                         >
-                                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                          <CheckIcon
+                                            className="w-5 h-5"
+                                            aria-hidden="true"
+                                          />
                                         </span>
                                       ) : null}
                                     </>
@@ -164,17 +198,20 @@ export default function Index({ }) {
                   </div>
                   <div className="py-2">
                     <h1 className="mb-2 text-sm">Lokasi</h1>
-                    <Combobox value={selectedRegion} onChange={setSelectedRegion}>
+                    <Combobox
+                      value={selectedRegion}
+                      onChange={setSelectedRegion}
+                    >
                       <div className="relative mt-1">
-                        <div className="relative w-full cursor-default overflow-hidden rounded bg-white text-left border border-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                        <div className="overflow-hidden relative w-full text-left bg-white rounded border border-gray-300 cursor-default sm:text-sm focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300">
                           <Combobox.Input
-                            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                            className="py-2 pr-10 pl-3 w-full text-sm leading-5 text-gray-900 border-none focus:ring-0"
                             displayValue={(reg) => reg.name}
                             onChange={(event) => setQuery(event.target.value)}
                           />
-                          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                          <Combobox.Button className="flex absolute inset-y-0 right-0 items-center pr-2">
                             <SelectorIcon
-                              className="h-5 w-5 text-gray-400"
+                              className="w-5 h-5 text-gray-400"
                               aria-hidden="true"
                             />
                           </Combobox.Button>
@@ -184,11 +221,11 @@ export default function Index({ }) {
                           leave="transition ease-in duration-100"
                           leaveFrom="opacity-100"
                           leaveTo="opacity-0"
-                          afterLeave={() => setQuery('')}
+                          afterLeave={() => setQuery("")}
                         >
-                          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {filteredRegion.length === 0 && query !== '' ? (
-                              <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                          <Combobox.Options className="overflow-auto absolute z-10 py-1 mt-1 w-full max-h-60 text-base bg-white rounded-md ring-1 ring-black ring-opacity-5 shadow-lg sm:text-sm focus:outline-none">
+                            {filteredRegion.length === 0 && query !== "" ? (
+                              <div className="relative py-2 px-4 text-gray-700 cursor-default select-none">
                                 Nothing found.
                               </div>
                             ) : (
@@ -196,7 +233,10 @@ export default function Index({ }) {
                                 <Combobox.Option
                                   key={reg.id}
                                   className={({ active }) =>
-                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                      active
+                                        ? "bg-blue-500 text-white"
+                                        : "text-gray-900"
                                     }`
                                   }
                                   value={reg}
@@ -204,17 +244,26 @@ export default function Index({ }) {
                                   {({ selected, active }) => (
                                     <>
                                       <span
-                                        className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                          }`}
+                                        className={`block truncate ${
+                                          selected
+                                            ? "font-medium"
+                                            : "font-normal"
+                                        }`}
                                       >
                                         {reg.name}
                                       </span>
                                       {selected ? (
                                         <span
-                                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-blue-500'
-                                            }`}
+                                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                            active
+                                              ? "text-white"
+                                              : "text-blue-500"
+                                          }`}
                                         >
-                                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                          <CheckIcon
+                                            className="w-5 h-5"
+                                            aria-hidden="true"
+                                          />
                                         </span>
                                       ) : null}
                                     </>
@@ -247,7 +296,7 @@ export default function Index({ }) {
           </section>
         </div>
         <FootNav />
-      </div >
+      </div>
     </>
-  )
+  );
 }
