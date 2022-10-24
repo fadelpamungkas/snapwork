268a78eafdeec9b6ab9e6108f150582ffc3f355d
fadelpamungkas <fadelpamungkas3@gmail.com>
2022-10-24 18:02:58 +0700

    Choose role now on independent page

--- pages/company/[companyid]/[jobId]/index.js
+++ pages/company/[companyid]/[jobId]/index.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import HeaderImage from "../../../../public/CompanyHeaderDefault.png";
 import FootNav from "../../../../components/FootNav";
 import HeadNav from "../../../../components/HeadNav";
-import ChooseRoleDialog from "../../../../components/ChooseRoleDialog";
 import TokopediaAvatar from "../../../../public/avtokopedia.png";
 import { BookmarkIcon } from "@heroicons/react/outline";
 import { Dialog, Transition } from "@headlessui/react";
@@ -26,7 +25,6 @@ export default function Index({ user }) {
   );
 
   let [isOpen, setIsOpen] = useState(false);
-  let [isOpenChooseUser, setIsOpenChooseUser] = useState(false);
 
   function closeModal() {
     setIsOpen(false);
@@ -38,7 +36,7 @@ export default function Index({ user }) {
 
   function openModal() {
     if (user.userData.role === "none") {
-      setIsOpenChooseUser(true);
+			router.push("/chooserole");
     } else {
       setIsOpen(true);
     }
@@ -332,69 +330,6 @@ export default function Index({ user }) {
                       type="button"
                       className="inline-flex justify-center py-2 px-4 text-sm font-medium text-blue-900 bg-blue-100 rounded-md border border-transparent hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
-                    >
-                      OK
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-      <Transition appear show={isOpenChooseUser} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          onClose={closeChooseUserModal}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="overflow-y-auto fixed inset-0">
-            <div className="flex justify-center items-center p-4 min-h-full text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="overflow-hidden p-8 w-full max-w-lg text-left align-middle bg-white rounded-2xl transition-all transform">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Choose User
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500 whitespace-pre-line">
-                      Anda berhasil mengisi lamaran pada{" "}
-                      <span className="font-bold">
-                        {" "}
-                        Application Designer - PT. Tokopedia Indonesia.{" "}
-                      </span>
-                      Anda dapat memantau proses rekrutmen melalui halaman{" "}
-                      <span className="text-blue-500"> Profil Saya.</span>
-                    </p>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center py-2 px-4 text-sm font-medium text-blue-900 bg-blue-100 rounded-md border border-transparent hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeChooseUserModal}
                     >
                       OK
                     </button>
