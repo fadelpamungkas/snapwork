19d6942f5649bc750a718baabe7f405aa283f168
fadelpamungkas <fadelpamungkas3@gmail.com>
2022-11-18 04:16:18 +0700

    Update HeadNav

--- components/HeadNav.js
+++ components/HeadNav.js
@@ -3,46 +3,72 @@ import useUser from "../lib/useUser";
 import {
   MapIcon,
   CubeTransparentIcon,
+  TrendingUpIcon,
+  ChevronDownIcon,
 } from "@heroicons/react/solid";
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import BerandaIcon from "../public/Beranda.svg";
+import BeritaIcon from "../public/Berita.svg";
+import TesIcon from "../public/Tes.svg";
 import ProfileMenu from "./ProfileMenu";
 
 export default function HeadNav() {
+  const solutions = [
+    {
+      name: "Selamat! Lamaran anda diterima",
+      description: "17 Oktober 2021 pukul 09:15",
+      href: "##",
+      icon: BerandaIcon,
+    },
+    {
+      name: "Maaf, Lamaran anda ditolak",
+      description: "9 September 2021 pukul 10.08",
+      href: "##",
+      icon: BeritaIcon,
+    },
+    {
+      name: "Reports",
+      description: "Keep track of your growth",
+      href: "##",
+      icon: TesIcon,
+    },
+  ];
   const { user } = useUser();
 
   console.log(user);
   return (
     <nav className="fixed top-0 z-10 w-full bg-gray-50 shadow-sm">
-      <div className="mx-auto flex max-w-screen-xl items-center justify-between py-4 px-8">
+      <div className="flex justify-between items-center py-4 px-8 mx-auto max-w-screen-xl">
         <Link href="/">
           <a className="text-xl font-medium tracking-wide">SnapWork</a>
         </Link>
-        <ul className="flex items-center justify-between space-x-4">
-          {/* <div className="w-full">
-            <input
-              id="search"
-              type="search"
-              name="q"
-              className=" w-full rounded-lg py-3.5 px-4 text-sm font-medium leading-none text-gray-800 placeholder-gray-400 shadow-sm transition duration-150 hover:shadow-md focus:border-0 focus:outline-none"
-              placeholder="Search for a service"
-            />
-          </div> */}
-          <li className="group flex items-center gap-x-2">
+        <ul className="flex justify-between items-center space-x-4">
+          <li className="flex gap-x-2 items-center group">
             <Link href="/">
-              <a className="text-md flex font-medium tracking-wider transition duration-150 group-hover:scale-110 group-hover:font-semibold">
-                <MapIcon className="mr-2 h-6 w-6 scale-0 rounded-lg bg-blue-500 fill-white p-1 transition duration-150 group-hover:scale-100" />
-                Beranda
+              <a className="flex items-center space-x-2 font-medium tracking-wider transition duration-150 group-hover:font-semibold group-hover:scale-105 text-md">
+                <BerandaIcon className="transition duration-150 scale-0 group-hover:scale-100" />
+                <h1>Beranda</h1>
               </a>
             </Link>
           </li>
-          <li className="group flex items-center gap-x-2">
-            <Link href="/about">
-              <a className="text-md flex font-medium tracking-wider transition duration-150 group-hover:scale-110 group-hover:font-semibold">
-                <MapIcon className="mr-2 h-6 w-6 scale-0 rounded-lg bg-blue-500 fill-white p-1 transition duration-150 group-hover:scale-100" />
-                Tentang Kami
+          <li className="flex gap-x-2 items-center group">
+            <Link href="/news">
+              <a className="flex items-center space-x-2 font-medium tracking-wider transition duration-150 group-hover:font-semibold group-hover:scale-105 text-md">
+                <BeritaIcon className="transition duration-150 scale-0 group-hover:scale-100" />
+                <h1>Berita</h1>
               </a>
             </Link>
           </li>
-          {/* <li className="group flex items-center gap-x-2">
+          <li className="flex gap-x-2 items-center group">
+            <Link href="/quiz/confirmation">
+              <a className="flex items-center space-x-2 font-medium tracking-wider transition duration-150 group-hover:font-semibold group-hover:scale-105 text-md">
+                <TesIcon className="transition duration-150 scale-0 group-hover:scale-100" />
+                <h1>Tes Kepribadian</h1>
+              </a>
+            </Link>
+          </li>
+          <li className="flex gap-x-2 items-center group">
             <Popover className="relative">
               {({ open }) => (
                 <>
@@ -51,11 +77,12 @@ export default function HeadNav() {
                 ${open ? "" : "text-opacity-90"}
                 text-md flex items-center font-medium tracking-wider transition duration-150 group-hover:scale-110 group-hover:font-semibold`}
                   >
-                    <TrendingUpIcon className=" mr-2 h-6 w-6 scale-0 rounded-lg bg-blue-500 fill-white p-1 transition duration-150 group-hover:scale-100" />
-                    <span>Tentang Kami</span>
+                    <TrendingUpIcon className="p-1 mr-2 w-6 h-6 bg-blue-500 rounded-lg transition duration-150 scale-0 group-hover:scale-100 fill-white" />
+                    <span>Notifikasi</span>
                     <ChevronDownIcon
-                      className={`${open ? "-rotate-180 transform" : "text-opacity-70"
-                        }
+                      className={`${
+                        open ? "-rotate-180 transform" : "text-opacity-70"
+                      }
                   ml-1 h-5 w-5 text-black transition duration-150 ease-in-out group-hover:text-blue-500 group-hover:text-opacity-80`}
                       aria-hidden="true"
                     />
@@ -69,49 +96,31 @@ export default function HeadNav() {
                     leaveFrom="opacity-100 translate-y-0"
                     leaveTo="opacity-0 translate-y-1"
                   >
-                    <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
-                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                        <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                    <Popover.Panel className="absolute left-1/2 z-10 px-4 mt-3 w-screen max-w-lg transform -translate-x-1/2">
+                      <div className="overflow-hidden rounded-lg ring-1 ring-black ring-opacity-5 shadow-lg">
+                        <div className="relative p-7 bg-white space-y-0">
+                          <h1 className="pb-4 text-xl font-medium">
+                            Notifikasi
+                          </h1>
                           {solutions.map((item) => (
                             <a
                               key={item.name}
                               href={item.href}
-                              className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-50"
+                              className="flex items-center p-2 -m-3 rounded-lg transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-50"
                             >
-                              <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                                {/* <item.icon aria-hidden="true" />
-                                <Image
-                                  src={item.image}
-                                  width={35}
-                                  height={35}
-                                  alt={item.title}
-                                />
+                              <div className="flex flex-shrink-0 justify-center items-center w-10 h-10 text-white sm:w-12 sm:h-12">
+                                <item.icon aria-hidden="true" />
                               </div>
                               <div className="ml-4">
-                                <p className="text-sm font-medium text-gray-900">
+                                <p className="text-base font-medium text-gray-900">
                                   {item.name}
                                 </p>
-                                <p className="text-sm text-gray-500">
+                                <p className="text-base text-gray-500">
                                   {item.description}
                                 </p>
                               </div>
                             </a>
                           ))}
-                        </div>
-                        <div className="bg-gray-50 p-4">
-                          <a
-                            href="##"
-                            className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-50"
-                          >
-                            <span className="flex items-center">
-                              <span className="text-sm font-medium text-gray-900">
-                                Documentation
-                              </span>
-                            </span>
-                            <span className="block text-sm text-gray-500">
-                              Start integrating products and tools
-                            </span>
-                          </a>
                         </div>
                       </div>
                     </Popover.Panel>
@@ -119,15 +128,15 @@ export default function HeadNav() {
                 </>
               )}
             </Popover>
-          </li> */}
-          <li className="group flex items-center gap-x-2">
-            <Link href="/faq">
-              <a className="text-md flex font-medium tracking-wider transition duration-150 group-hover:scale-110 group-hover:font-semibold">
-                <CubeTransparentIcon className="mr-2 h-6 w-6 scale-0 rounded-lg bg-blue-500 fill-white p-1 transition duration-150 group-hover:scale-100" />
-                FAQ
+          </li>
+          {/* <li className="flex gap-x-2 items-center group">
+            <Link href="/about">
+              <a className="flex font-medium tracking-wider transition duration-150 group-hover:font-semibold group-hover:scale-110 text-md">
+                <CubeTransparentIcon className="p-1 mr-2 w-6 h-6 bg-blue-500 rounded-lg transition duration-150 scale-0 group-hover:scale-100 fill-white" />
+                About
               </a>
             </Link>
-          </li>
+          </li> */}
         </ul>
         <div className="space-x-6">
           {/* {`${user?.isLoggedIn ? user.userData.name : "guest"}`} */}
@@ -136,12 +145,12 @@ export default function HeadNav() {
           ) : (
             <>
               <Link href="/login">
-                <a className="rounded-xl px-6 py-3 font-medium text-blue-500 transition duration-150 hover:text-blue-600">
+                <a className="py-3 px-6 font-medium text-blue-500 rounded-xl transition duration-150 hover:text-blue-600">
                   Sign In
                 </a>
               </Link>
               <Link href="/signup">
-                <a className="rounded-xl bg-blue-500 px-6 py-3 font-medium text-white transition duration-150 hover:bg-blue-600">
+                <a className="py-3 px-6 font-medium text-white bg-blue-500 rounded-xl transition duration-150 hover:bg-blue-600">
                   Sign Up
                 </a>
               </Link>
